3e8e1de3673941cf6eec591a8c84cf16e0eee85a
Sameer Puri <sameer@users.noreply.github.com>
2020-03-19 18:08:43 -0400

    Updates

---
+++ precache-manifest.ac15c52fa10322da6d0941259809eece.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac17cdd1facfa9ded416",
+    "url": "/zstack-mobile/static/js/app.79e3209c.chunk.js"
+  },
+  {
+    "revision": "635d8771c2b2b4dae80d",
+    "url": "/zstack-mobile/static/js/runtime~app.418b8f3b.js"
+  },
+  {
+    "revision": "264522f9c32fe9685793",
+    "url": "/zstack-mobile/static/js/2.8e2cdb3e.chunk.js"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/zstack-mobile/serve.json"
+  },
+  {
+    "revision": "7a7bc7ead25db795e58b336f04d2624c",
+    "url": "/zstack-mobile/favicon.ico"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/zstack-mobile/expo-service-worker.js"
+  },
+  {
+    "revision": "fb1bd367f7d62c45b9e29e3a939082f5",
+    "url": "/zstack-mobile/manifest.json"
+  },
+  {
+    "revision": "ef58ff9a14b1871e5232fada458806e5",
+    "url": "/zstack-mobile/apple/icons/icon_180x180.png"
+  },
+  {
+    "revision": "dd0d8974f196283ab21f6bb647882750",
+    "url": "/zstack-mobile/apple/icons/icon_192x192.png"
+  },
+  {
+    "revision": "b370d9dab464db3e849d854262b5d2e5",
+    "url": "/zstack-mobile/apple/icons/icon_512x512.png"
+  },
+  {
+    "revision": "b4c95fe40cc182ee41709b5386a65a60",
+    "url": "/zstack-mobile/apple/splash/icon_1125x2436.png"
+  },
+  {
+    "revision": "24057a53b4b8207ac6128f5a1858a41f",
+    "url": "/zstack-mobile/apple/splash/icon_1136x640.png"
+  },
+  {
+    "revision": "d8bfb76284287658cab36f271d2a2a4a",
+    "url": "/zstack-mobile/apple/splash/icon_1242x2208.png"
+  },
+  {
+    "revision": "46f33d43a6ee160cfb767b7e565d3963",
+    "url": "/zstack-mobile/apple/splash/icon_1242x2688.png"
+  },
+  {
+    "revision": "ff41747f4f3bb2f9a29c2ee494815852",
+    "url": "/zstack-mobile/apple/splash/icon_1334x750.png"
+  },
+  {
+    "revision": "79a7a4869dc12a14e286c382d496dfd0",
+    "url": "/zstack-mobile/apple/splash/icon_1792x828.png"
+  },
+  {
+    "revision": "7e9fc39b1c37a328126323bbe8cd404b",
+    "url": "/zstack-mobile/apple/splash/icon_2208x1242.png"
+  },
+  {
+    "revision": "1fd86c39fec0a11f2f6b61d5a7af0f39",
+    "url": "/zstack-mobile/apple/splash/icon_2436x1125.png"
+  },
+  {
+    "revision": "dbbf9209e64340c9d1b78823b8f8abf2",
+    "url": "/zstack-mobile/apple/splash/icon_2688x1242.png"
+  },
+  {
+    "revision": "df18c0c33e95b50b62f822d65cd9b909",
+    "url": "/zstack-mobile/apple/splash/icon_640x1136.png"
+  },
+  {
+    "revision": "5d0bcf1c916780f7a20f047bb6d8a30c",
+    "url": "/zstack-mobile/apple/splash/icon_750x1334.png"
+  },
+  {
+    "revision": "4ed2cf28de535595662c61587b07aa56",
+    "url": "/zstack-mobile/apple/splash/icon_828x1792.png"
+  },
+  {
+    "revision": "3d3b05b24633ccfa3b14a69f70f2dc8b",
+    "url": "/zstack-mobile/index.html"
+  },
+  {
+    "revision": "099df4895d673dabd31bbb0de8e7663a",
+    "url": "/zstack-mobile/register-service-worker.js"
+  }
+];
